67befefce25b26abce01c6ca00544254e8b424b7
cuac <you@example.com>
2019-12-05 22:03:54 -0600

    inicio

---
+++ index.js
@@ -0,0 +1,31 @@
+const toursMx = {
+    id: 10,
+    slug: "mexico",
+    nombreTour: "Juan Tours",
+    operador: "Juan",
+    tipoDeTour: "Tour por la playa",
+    descripcion: "Podras conocer unas playas hermosas que se encuentran en Mexico",
+    img: 'alguna img',
+    pais: "Mexico",
+    zonaLlegada: "Yucatan",
+    zonaSalida: "Ciudad de Mexico",
+    escalas: ['ciudad de Mexico', 'Yucatan'],
+    acomodacionIncluida: true,
+    transporteIncluido: true,
+    edadMinima: 18,
+    requisitos: {Object
+            seguroDeViaje: true
+        };
+    duracionTour: 20,
+    opiniones: null,
+    calificacion: null,
+    precioUSD: 600,
+    fechasDisponibles: Object,
+    - "2019": Array of Objects        
+        - id: String,
+        - fechaInicio: String,
+        - fechaFin: String
+        - asientosDisponibles: Number,
+        - asientosReservados: Number,
+        - precioRealUSD: Number
+}
